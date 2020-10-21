75faa00857cd6d8c5c9e608d1a7ab732f0285976
First Baptist Church of Altoona <fbcaltoona@MacBook-Pro.local>
2020-10-20 20:42:42 -0400

    Updated PS Script

--- psOBS.ps1
+++ psOBS.ps1
@@ -1,25 +1,36 @@
 # This script is intended to gather all relevant local data
 # regarding connected devices, and set up an OBS profile/scene
 # collection with those details.
+begin{
+  #region Import Modules
+    Import-Module $PSScriptRoot\submodules\PsIni\PsIni
+  #endregion
 
-$connectedCameras = (Invoke-Command -ScriptBlock {system_profiler SPCameraDataType -json 2> /dev/null} | ConvertFrom-Json).SPCameraDataType
-
-$selectedCamera = $connectedCameras | Where-object {$_.'_name' -like "PTZ Optics*"} | Select-object -First 1
-
-if (-not $selectedCamera) {
-  Clear-Host
-  $selectAltCam = Read-Host -Prompt "No 'PTZ Camera' found.  Would you like to use $($connectedCameras[0]._name)? [ yes | no ]"
-  Clear-Host
-  if ($selectAltCam -like "y*") {
-    $selectedCamera = $connectedCameras[0]
-  } else {
-    Write-Host "Ok. please connect the camera and try again... Exiting..."
-    Start-sleep -seconds 5
-    exit
-  }
 }
-Write-Host $selectedCamera._name
-Write-Host "Loading Default Config..."
 
-# Get default Config
-$sceneCollection = Get-Content .\fbc-SceneCollectionDefault.json | ConvertFrom-Json
+process{
+  $connectedCameras = (Invoke-Command -ScriptBlock {system_profiler SPCameraDataType -json 2> /dev/null} | ConvertFrom-Json).SPCameraDataType
+
+  $selectedCamera = $connectedCameras | Where-object {$_.'_name' -like "PTZ Optics*"} | Select-object -First 1
+
+  if (-not $selectedCamera) {
+    Clear-Host
+    $selectAltCam = Read-Host -Prompt "No 'PTZ Camera' found.  Would you like to use $($connectedCameras[0]._name)? [ yes | no ]"
+    Clear-Host
+    if ($selectAltCam -like "y*") {
+      $selectedCamera = $connectedCameras[0]
+    } else {
+      Write-Host "Ok. please connect the camera and try again... Exiting..."
+      Start-sleep -seconds 5
+      exit
+    }
+  }
+  Write-Host $selectedCamera._name
+  Write-Host "Loading Default Config..."
+
+  # Get default Config
+  $sceneCollection = Get-Content .\fbc-SceneCollectionDefault.json | ConvertFrom-Json
+}
+end{
+  Write-Host "Exiting"
+}
